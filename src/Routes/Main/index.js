f0df7df476461dc482eafc852ccca4f643ee3831
HYUNAHSHIM <hyunahshim@naver.com>
2020-11-24 04:05:14 +0900

    [feat] 컴포넌트: 밑줄 input component 추가

--- src/Routes/Main/index.js
+++ src/Routes/Main/index.js
@@ -1,3 +1,4 @@
 import React from "react";
+import LineInput from "../../Components/LineInput";
 
-export default () => <div>hi</div>;
+export default () => <LineInput placeholder={"hi"} icon="delete" onChange={() => {}} width={"300px"}/>;
