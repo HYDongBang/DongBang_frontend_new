b51fa18fc5d5c2fd9dcc4c9a41ef60e88fd61de7
HYUNAHSHIM <hyunahshim@naver.com>
2020-11-23 20:33:22 +0900

    [feat] 초기화: 기본틀 구성

---
+++ src/Apollo/Client.js
@@ -0,0 +1,18 @@
+import ApolloClient from "apollo-boost";
+import { defaults, resolvers } from "./LocalState";
+
+export default new ApolloClient({
+    uri: "http://18.210.10.66:4000",
+    clientState: {
+        defaults,
+        resolvers
+    },
+    request: async operation => {
+        const token = await localStorage.getItem("token");
+        operation.setContext({
+            headers: {
+                Authorization: token ? `Bearer ${token}` : ""
+            }
+        });
+    }
+});
