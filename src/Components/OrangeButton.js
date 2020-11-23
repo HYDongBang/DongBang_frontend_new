37de1bbe6b1a314d56267b4cd5cb1be7b2b44bd3
RunaNam <nmj5012@naver.com>
2020-11-24 00:23:25 +0900

    [feat]컴포넌트:OrangeButton

--- src/Components/OrangeButton.js
+++ src/Components/OrangeButton.js
@@ -1,13 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-
+//현재 폰트가 잘 안먹힘.. 
 const ButtonContainer = styled.div`
   width: 256px;
   height: 59px;
   border-radius: 38px;
   background-color:${props=>props.theme.orange};
   text-align:center;
+  line-height:3.5;
+  font-family:'raleBold';
+  color:${props=>props.theme.white};
 `;
 
 
