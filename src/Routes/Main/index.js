9071747f9db16a29e8eedab1997cd735d3be5094
RunaNam <nmj5012@naver.com>
2020-11-25 02:29:07 +0900

    [feat]컴포넌트 : ClubInfoBUtton 추가

--- src/Routes/Main/index.js
+++ src/Routes/Main/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ClubInfoButton from "../../Components/ClubInfobutton";
 import ClubLogo from "../../Components/ClubLogo";
 import OrangeButton from "../../Components/OrangeButton";
 import TopButton from "../../Components/TopButton";
@@ -12,4 +13,7 @@ export default () => <div>hi
 <UserLogo name="남" />
 <UserLogo size="55px" name="남" font="26px"/>
 <UserLogo size="50px" name="남" font="22px"/>
+<ClubInfoButton content="지원하기"/>
+<ClubInfoButton content="실시간 톡"/>
+<ClubInfoButton content="동아리소개"/>
 </div>;
