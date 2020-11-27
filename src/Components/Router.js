9f41158232516f02643528a746f7c18de4087ab1
HYUNAHSHIM <hyunahshim@naver.com>
2020-11-28 00:12:12 +0900

    [fix] 폰트: 폰트 파일 이름 수정

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,12 +3,14 @@ import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom"
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import Main from "../Routes/Main";
+import My from "../Routes/My";
 
 const AppRouter = () => (
     <Router>
         <Header />
         <Switch>
             <Route path="/" exact component={Main} />
+            <Route path="/My" component={My} />
             <Redirect path="*" to="/" />
         </Switch>
         <Footer />
