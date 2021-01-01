bcb9c886cc1f14d3b8fe99120d1bbb69e87332c3
RunaNam <nmj5012@naver.com>
2021-01-02 01:40:21 +0900

    [style] Clubinfo 수정(아직 버그 있음)

--- src/Routes/MJ_page/ClubInfo/InfoPresenter.js
+++ src/Routes/MJ_page/ClubInfo/InfoPresenter.js
@@ -37,9 +37,8 @@ const Buttons = styled.div`
 
 const Club = styled.div`
     text-align:center;
-    position:absolute;
-    top:39%;
-    left:47%;
+    width:fit-content;
+    margin:-47px auto 0px;
 `;
 
 const Type = styled.div`
@@ -53,7 +52,7 @@ const Name = styled.div`
 `;
 
 const Desc = styled.div`
- padding: 100px 7% 0px 7%;
+ padding: 25px 7% 0px 7%;
  line-height: 1.5em;
  font-size:0.9em;
 `;
@@ -140,7 +139,13 @@ export default ({club, setAction, action})=>{
 
 return (
     <>
-        <HeaderImg src = {headerMovie}/>
+        <HeaderImg src = {headerMovie}>
+        </HeaderImg>
+        <Club>
+            <ClubLogo style={{margin:"auto"}} type = "sports"/>
+            <Type>체육</Type>
+            <Name>HYSCUBA</Name>
+        </Club>
         <Buttons>
             <ButtonContainer onClick={() => setAction("Talk")} >
                 <FontAwesomeIcon  style ={{marginRight:"2%"}} size="1.5x" icon={faComments} />   
@@ -151,16 +156,8 @@ return (
                 <FontAwesomeIcon  style ={{marginRight:"2%"}} size="1.5x"  icon={faPaperPlane}  />   
                 지원하기
             </ButtonContainer>
-
-            {/* <ClubInfoButton content = "실시간 톡" onClick={() => setAction("Talk")}/> */}
-            {/* <ClubInfoButton content = "지원하기" onClick={() => setAction("Apply")}/> */}
         </Buttons>
 
-        <Club>
-            <ClubLogo style={{margin:"auto"}} type = "sports"/>
-            <Type>체육</Type>
-            <Name>HYSCUBA</Name>
-        </Club>
         <Desc> 
             스포츠 다이빙은 레크레이션 다이빙 활동을 대표한다. 스포츠 다이빙은 많은 사람들이 즐기고 있으며, 따뜻하고 이국적인 지역에서 간단한 장비와 초급 단계의 훈련을 받고도 충분히 즐길 수 있다.
             반면 스포츠 다이빙의 한계와 제한 범위는 명확하게 정의되어 있다. 장비는 실린더(탱크) 한 개를 사용해야 하고, 최대 허용 수심은 40m지만, PADI의 오픈워터 레벨을 기준으로 초급 다이버는 한계수심이 18m로 제한되며 천정이 막힌 환경(예를 들면 수중동굴이나 난파선의 내부 등)에서는 다이빙할 수 없다. 초급 라이센스 다이버일수록 트러블 발생시 대처능력이 떨어지고, 긴급 상황에서 수면으로 상승해야할 시 위가 막힌 환경이라면 사고로 이어질 수 있기 때문이다. 초급 레벨을 벗어나 어드밴스로 레벨을 올리면 한계수심이 최대40m까지 허용이 되며 위가 막힌 환경에서도 다이빙할 수 있도록 교육받게 된다. 동시에 대부분의 스포츠 다이빙에서는 무감압 다이빙을 한다.
