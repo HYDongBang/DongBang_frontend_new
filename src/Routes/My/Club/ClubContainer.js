4b5be42a2de2d6b583f1797212d42b9ef75b7c28
HYUNAHSHIM <hyunahshim@naver.com>
2021-01-09 03:23:09 +0900

    [feat] Club 페이지: 서버 연동 - 카테고리 변경 가능하게 기능 추가

--- src/Routes/My/Club/ClubContainer.js
+++ src/Routes/My/Club/ClubContainer.js
@@ -6,7 +6,7 @@ import { GET_CLUB, UPDATE_CLUB } from "./ClubQueries";
 
 export default () => {
     const getClubQuery = useQuery(GET_CLUB);
-    const [ updateClubMutation ] = useMutation(UPDATE_CLUB);
+    const [updateClubMutation] = useMutation(UPDATE_CLUB);
     const name = useInput("");
     const type = useInput("");
     const description = useInput("");
@@ -23,13 +23,27 @@ export default () => {
     const facebookUrl = useInput("");
 
     useEffect(() => {
-        if(getClubQuery.data) {
-            const info = getClubQuery.data.readLoggedInUser;
-            console.log(info);
+        if (getClubQuery.data) {
+            const info = getClubQuery.data.readLoggedInUser.clubMaster;
+            name.setValue(info.name);
+            type.setValue(info.type);
+            description.setValue(info.description);
+            content.setValue(info.content);
+            clubImage.setValue(info.clubImage);
+            logoImage.setValue(info.logoImage);
+            partyDay.setValue(info.partyDay);
+            party.setValue(info.party);
+            numberOfMembers.setValue(info.numberOfMembers);
+            isUnion.setValue(info.isUnion);
+            email.setValue(info.email);
+            phoneNumber.setValue(info.phoneNumber);
+            instagramUrl.setValue(info.instagramUrl);
+            facebookUrl.setValue(info.facebookUrl);
         }
     }, [getClubQuery.data]);
 
-    const onImgClick = (close) => {
+    const onImgClick = (close, id) => {
+        type.setValue(id);
         close();
     };
 
@@ -38,8 +52,8 @@ export default () => {
         try {
             const { data } = await updateClubMutation({
                 variables: {
-                    name: name,
-                    type: type,
+                    name: name.value,
+                    type: type.value,
                     description: description.value,
                     content: content.value,
                     clubImage: clubImage.value,
@@ -51,34 +65,38 @@ export default () => {
                     email: email.value,
                     phoneNumber: phoneNumber.value,
                     instagramUrl: instagramUrl.value,
-                    facebookUrl: facebookUrl.value,
+                    facebookUrl: facebookUrl.value
                 }
             });
-            if(!data) {
-                console.error("faile to edit club profile");
+            if (!data) {
+                console.error("fail to edit club profile");
             } else {
-                alert("동아리 정보를 수정하였습니다.")
+                alert("동아리 정보를 수정하였습니다.");
             }
         } catch (e) {
             console.log(e.message);
             alert("다시 시도해 주세요.");
         }
-    }
-
-    return <ClubPresenter name={name}
-    type={type}
-    description={description}
-    content={content}
-    clubImage={clubImage}
-    logoImage={logoImage}
-    partyDay={partyDay}
-    party={party}
-    numberOfMembers={numberOfMembers}
-    isUnion={isUnion}
-    email={email}
-    phoneNumber={phoneNumber}
-    instagramUrl={instagramUrl}
-    facebookUrl={facebookUrl} 
-    onImgClick={onImgClick} 
-    onSubmit={onSubmit}/>;
+    };
+    return (
+        <ClubPresenter
+            name={name}
+            type={type}
+            description={description}
+            content={content}
+            clubImage={clubImage}
+            logoImage={logoImage}
+            partyDay={partyDay}
+            party={party}
+            numberOfMembers={numberOfMembers}
+            isUnion={isUnion}
+            email={email}
+            phoneNumber={phoneNumber}
+            instagramUrl={instagramUrl}
+            facebookUrl={facebookUrl}
+            onImgClick={onImgClick}
+            onSubmit={onSubmit}
+            loading={getClubQuery.loading}
+        />
+    );
 };
