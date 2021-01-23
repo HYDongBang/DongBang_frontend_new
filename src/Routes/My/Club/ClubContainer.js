862dd51890c2d960844badc8882de047728f64d9
HYUNAHSIM <hyunso8245@gmail.com>
2021-01-24 03:05:48 +0900

    [fix] Member 페이지 직급 변경 오류 수정

--- src/Routes/My/Club/ClubContainer.js
+++ src/Routes/My/Club/ClubContainer.js
@@ -3,11 +3,12 @@ import axios from "axios";
 import ClubPresenter from "./ClubPresenter";
 import useInput from "../../../Hooks/useInput";
 import { useMutation, useQuery } from "react-apollo-hooks";
-import { GET_CLUB, UPDATE_CLUB } from "./ClubQueries";
+import { GET_CLUB, UPDATE_CLUB, CREATE_POST } from "./ClubQueries";
 
 export default () => {
     const getClubQuery = useQuery(GET_CLUB);
     const [updateClubMutation] = useMutation(UPDATE_CLUB);
+    const [createPostMutation] = useMutation(CREATE_POST);
     const name = useInput("");
     const type = useInput("");
     const description = useInput("");
@@ -24,6 +25,10 @@ export default () => {
     const phoneNumber = useInput("");
     const instagramUrl = useInput("");
     const facebookUrl = useInput("");
+    const title = useInput("");
+    const postContent = useInput("");
+    const postImage = useInput("");
+    const postFile = useInput(null);
 
     useEffect(() => {
         if (getClubQuery.data) {
@@ -59,6 +64,9 @@ export default () => {
         } else if (id === "clubImage") {
             clubFile.setValue(file);
             clubImage.setValue(file.name);
+        } else if (id === "postImage") {
+            postFile.setValue(file);
+            postImage.setValue(file.name);
         }
     };
 
@@ -124,6 +132,35 @@ export default () => {
             alert("다시 시도해 주세요.");
         }
     };
+
+    const onClickPost = async e => {
+        try {
+            let postLocation = "";
+            const file = new FormData();
+            file.append("file", postFile.value);
+            {
+                const { data } = await axios.post("http://ec2-52-79-235-57.ap-northeast-2.compute.amazonaws.com:4000/api/upload", file, {
+                    headers: {
+                    "content-type": "multipart/form-data"
+                    }
+                });
+                postLocation = data.location;
+            }
+            const { data } = await createPostMutation({
+                variables: {
+                    title: title.value,
+                    content: postContent.value,
+                    fileUrl: postLocation
+                }
+            });
+            alert("컨텐츠를 추가하였습니다.");
+            window.location.reload();
+        } catch (e) {
+            console.log(e.message);
+            alert("다시 시도해 주세요.");
+        }
+    };
+
     return (
         <ClubPresenter
             name={name}
@@ -140,10 +177,14 @@ export default () => {
             phoneNumber={phoneNumber}
             instagramUrl={instagramUrl}
             facebookUrl={facebookUrl}
+            title={title}
+            postContent={postContent}
+            postImage={postImage}
             onImgClick={onImgClick}
             onSubmit={onSubmit}
             onFileUpload={onFileUpload}
             onClickRadio={onClickRadio}
+            onClickPost={onClickPost}
             loading={getClubQuery.loading}
         />
     );
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -2,41 +2,53 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 export default createGlobalStyle`
-${reset};
+  ${reset};
 
   @font-face {
     font-family:'spoHanR';
     src: url(${require("./Font/Spoqa/Spoqa-Han-Sans-Regular.ttf")});
-  }
+  } 
   @font-face {
     font-family:'spoHanB';
     src: url(${require("./Font/Spoqa/Spoqa-Han-Sans-Bold.ttf")});
-  }
+  } 
   @font-face {
     font-family:'raleRegular';
     src: url(${require("./Font/Raleway/Raleway-Regular.ttf")});
-  }
+  } 
   @font-face {
     font-family:'raleBold';
     src: url(${require("./Font/Raleway/Raleway-Bold.ttf")});
-  }
+  } 
+  @font-face {
+    font-family:NanumGothicR;
+    src: url(${require("./Font/Nanum_Gothic/NanumGothic-Regular.ttf")});
+  } 
+  @font-face {
+    font-family:'Jua';
+    src: url(${require("./Font/Jua/Jua-Regular.ttf")});
+  } 
+  @font-face {
+    font-family:'NanumMyeonjo';
+    src: url(${require("./Font/Nanum_Myeongjo/NanumMyeongjo-Regular.ttf")});
+  } 
 
  *{
   box-sizing: border-box;
-}
-body {
-        background-color:${props => props.theme.white};
-        color:${props => props.theme.black};
-        font-family:'spoHanR';
-    }
-    a {
-        text-decoration:none;
-    }
-    input:focus{
-        outline:none;
-    }
-    a:hover{
-        text-decoration:none;
-    }
+  }
 
+  body {
+    background-color:${props => props.theme.white};
+    color:${props => props.theme.black};
+    font-family: NanumGothicR;
+    }
+  a {
+    text-decoration:none;
+  }
+  input:focus{
+    outline:none;
+  }
+  a:hover{
+    text-decoration:none;
+  }
 `;
